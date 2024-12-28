16aa28b147b3a28696773fed39a27fd1821c6118
FadhlanDU <fadlandu3@gmail.com>
2024-12-28 17:42:21 +0700

    second commit

--- app/Http/Controllers/API/ProfileController.php
+++ app/Http/Controllers/API/ProfileController.php
@@ -11,7 +11,7 @@ class ProfileController extends Controller
 
     public function __construct()
     {
-        $this->middleware(['auth:api', 'verfiedAccount']);
+        $this->middleware(['auth:api', 'verifiedAccount']);
     }
 
     public function storeupdate(Request $request) {
